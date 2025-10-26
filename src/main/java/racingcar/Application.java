31a8b82c43d1e50458de9eb904872a5b42bcb887
joohyeongzz81 <joohyeongzz81@gmail.com>
2025-10-26 14:28:00 +0900

    feat(app): 메인 실행 로직 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,26 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<String> carNames = inputCarNames();
+        int roundCount = inputRoundCount();
+        
+        RacingGame game = new RacingGame(carNames, roundCount);
+        game.start();
+    }
+    
+    private static List<String> inputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        return InputValidator.parseCarNames(input);
+    }
+    
+    private static int inputRoundCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String input = Console.readLine();
+        return InputValidator.parseRoundCount(input);
     }
 }
